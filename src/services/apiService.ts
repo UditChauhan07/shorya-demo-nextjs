3601bff8670b93b03b517fe73fb27ed0256282dd
Shorya Verma <shoryaverma.dx@gmail.com>
2025-05-13 17:30:28 +0530

    [SV] code update (SV 1.1)

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,38 +1,39 @@
 import axios from "axios";
 export const Url = process.env.NEXT_PUBLIC_API_URL || "";
+// import d from "../app/api/auth/login/route"
 
 //Users Signup Api
-export const signupApi = async (finaldata:any) => {
+export const signupApi = async (finaldata: any) => {
   try {
-    const response = await axios.post(`${Url}/api/auth/signup`, finaldata, {
+    const response = await axios.post(`/api/auth/signup`, finaldata, {
       headers: {
         "Content-Type": "application/json",
       },
     });
     console.log(response.data, "Users api data");
     return response.data;
-  } catch (error:unknown) {
+  } catch (error: unknown) {
     if (axios.isAxiosError(error) && error.response) {
-        return error.response.data;
-      }
+      return error.response.data;
+    }
     throw new Error("An unexpected error occurred");
   }
 };
 
 // User Login Api
-export const LoginApi = async (finaldata:any) => {
-    try {
-      const response = await axios.post(`${Url}/api/auth/login`, finaldata, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(response.data, "Login api data");
-      return response.data;
-    } catch (error:unknown) {
-        if (axios.isAxiosError(error) && error.response) {
-            return error.response.data;
-          }
-      throw new Error("An unexpected error occurred");
+export const LoginApi = async (finaldata: any) => {
+  try {
+    const response = await axios.post(`/api/auth/login`, finaldata, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response.data, "Login api data");
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data;
     }
-  };
+    throw new Error("An unexpected error occurred");
+  }
+};
